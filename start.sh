bccc087f968c1822bbff64a1beffa01f7c849773
DCsunset <dcsunset@protonmail.com>
2020-05-16 20:47:58 -0700

    feat: add xstartup script and config

--- start.sh
+++ start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 umask 0077                # use safe default permissions
-mkdir -p "$HOME/.vnc"     # create config directory
 chmod go-rwx "$HOME/.vnc" # enforce safe permissions
+chmod +x "$HOME/.vnc/xstartup"
+
+# chromium
+sed -i 's/\/usr\/bin\/chromium/\/usr\/bin\/chromium --no-sandbox/g' /usr/share/applications/chromium.desktop
 
 # Start TigerVNC
 if [ ! -z $VNC_PASSWD ]; then
